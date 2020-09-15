a1ea6935f61acdee052c6dd79fdb90ffff2f8aa6
Adriano Pimenta <apimenta.usp@gmail.com>
2020-09-15 10:29:31 -0300

    Acrescentando traços

--- ProjetoTestandoGit/Program.cs
+++ ProjetoTestandoGit/Program.cs
@@ -3,10 +3,12 @@
 namespace ProjetoTestandoGit {
     class Program {
         static void Main(string[] args) {
+            Console.WriteLine("--------------------------");
             Console.WriteLine("Olá Mundo!");
             Console.WriteLine("Como você está hoje?");
             Console.WriteLine("Tenha um bom dia!");
             Console.WriteLine("Tchau!");
+            Console.WriteLine("--------------------------");
 
             Console.ReadLine();
         }
