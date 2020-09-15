1659462b1d7a0b07e1fb555e7a21d989d6dd1fbf
Adriano Pimenta <apimenta.usp@gmail.com>
2020-09-15 09:02:17 -0300

    Projeto iniciado, mostrando a mensagem Ola Mundo

---
+++ ProjetoTestandoGit/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace ProjetoTestandoGit {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Olá Mundo!");
+            Console.ReadLine();
+        }
+    }
+}
