9150009022478edc34c0ca4c59470c2d14d1c9c9
Adriano Pimenta <apimenta.usp@gmail.com>
2020-09-15 10:03:20 -0300

    Acrescentada a mensagem desejando um bom dia

--- ProjetoTestandoGit/Program.cs
+++ ProjetoTestandoGit/Program.cs
@@ -4,8 +4,8 @@ namespace ProjetoTestandoGit {
     class Program {
         static void Main(string[] args) {
             Console.WriteLine("Olá Mundo!");
-            Console.WriteLine("Bom dia!");
             Console.WriteLine("Como você está hoje?");
+            Console.WriteLine("Tenha um bom dia!");
 
             Console.ReadLine();
         }
